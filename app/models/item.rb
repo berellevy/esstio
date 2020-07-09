bbe7ba6400234bbdec7568ebc10c43e9586cc39c
mhdhassan99 <mhassan.work99@gmail.com>
2020-07-09 10:25:50 -0400

    fully working resturant stories

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :restaurant
   has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
+
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: {greater_than: 0}
+  # validates_uniqueness_of name: {scope: :restaurant}
 end
